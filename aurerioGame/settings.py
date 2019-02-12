875f3429e230d1a56264f2e8d918e385f4accee0
Lucas <htslucasv@gmail.com>
2019-02-12 14:54:30 -0200

    Rodando o Home

--- aurerioGame/settings.py
+++ aurerioGame/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECTS_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = [u'LuscaCamilo.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'contas',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'aurerioGame.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['%s/templates/' % PROJECTS_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,4 +125,4 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = u'/home/LuscaCamilo/aurerioGame/media'
 MEDIA_URL = '/media/'
 STATIC_ROOT = u'/home/LuscaCamilo/aurerioGame/static'
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
