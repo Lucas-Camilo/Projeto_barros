0b039c5b3cc2b2339e75ca3b9a2af1504d53a3a5
Lucas <htslucasv@gmail.com>
2019-03-05 22:17:40 -0300

    subindo banco

--- contas/views.py
+++ contas/views.py
@@ -1,11 +1,34 @@
 from django.shortcuts import render
+from .models import Usuario
+
 
 # Create your views here.
 
 
 def home(request):
     return render(request, 'contas/home.html')
+
+
 def login(request):
     return render(request, 'contas/login.html')
+
+
 def registrar(request):
     return render(request, 'contas/registro.html')
+
+
+def novo_usuario(request):
+    nome = request.Get.get("nome")
+    pont = request.Get.get("pontuacao")
+    try:
+        usuario = Usuario.objects.filter(nome=nome)
+        if len(usuario == 1):
+            Usuario.objects.create(nome=nome, pontuacao=pont)
+        else:
+            nome = "{}({})".format(nome, len(usuario) + 1)
+            Usuario.objects.create(nome=nome, pontuacao=pont)
+
+    except:
+        pass
+    finally:
+        return render(request, "home.html")
